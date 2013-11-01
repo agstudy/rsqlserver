314bf3c96773b658ec22cc1167416394cdebe1bd
A.G <amingassem@gmail.com>
2013-11-01 13:27:53 +0100

    correct way to load dll in .onload

--- R/zzz.R
+++ R/zzz.R
@@ -3,13 +3,15 @@
 .onLoad <- function(libname, pkgname){
   if(.Platform$OS.type == "windows") {
     libLocation<- system.file(package=pkgname)
-    libpath <- file.path(libLocation, 'libs')
+   libpath <- file.path(libLocation, 'libs')
+   ## libpath <- file.path(libLocation)
     f <- file.path(libpath, 'rsqlserver.net.dll')
     if( !file.exists(f) ) {
       packageStartupMessage('Could not find path to rsqlserver.dll, 
                             you will have to load it manually')
     } else {
-      clrLoadAssembly(f)
+      clrLoadAssembly('System.Data') ## .net provider
+      clrLoadAssembly(f)                       ## custom dll 
     }
   }
 }
