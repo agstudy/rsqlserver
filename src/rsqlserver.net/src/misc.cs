bea797891cf373c28d3c3f54a2dbbacc2ca9ab4d
A.G <amingassem@gmail.com>
2013-11-04 23:28:34 +0100

    add new class for misc jobs.

---
+++ src/rsqlserver.net/src/misc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rsqlserver.net
+{
+    public class misc
+    {
+        public static Single[] GetSingleArray()
+        {
+            Single[] r = new Single[2] { Single.MaxValue, Single.MinValue };
+            return r;
+        }
+
+        public static double[] FromSingle2DoubleArray()
+        {
+            var r = GetSingleArray();
+            double[] dar = new double[r.Length];
+            for(int i =0;i < r.Length; i++)
+                dar[i] = (double)r[i];
+            return dar;
+        }
+
+    }
+}
