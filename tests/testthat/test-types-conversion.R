e72c1a0d4497b57176f4180e25c1c156162d06e0
A.G <amingassem@gmail.com>
2013-11-05 18:24:48 +0100

    add test if  the names are valid db names

--- tests/testthat/test-types-conversion.R
+++ tests/testthat/test-types-conversion.R
@@ -1,9 +1,22 @@
-# 
-# context("conversion of types")
-# 
-# test_that(" mapping is good between R and sql server types",
-# {
-#   
-#   expect_equal(db2RType("char"),"cahracter")
-#   
-# })
+
+context("conversion of types and db naming conventions")
+
+test_that("mapping is good between R and sql server types",
+{
+  
+  expect_equal(rsqlserver:::db2RType("char"),"character")
+  expect_equal(rsqlserver:::db2RType("varchar"),"character")
+  expect_equal(rsqlserver:::db2RType("int"),"integer")
+  
+})
+
+test_that("the names are valid db names",{
+  
+  drv <- dbDriver("SqlServer")
+  expect_equal(make.db.names(drv,"a mine"),"a_mine") ## space 
+  ## keywords 
+  expect_equal(make.db.names(drv,"create",allow.keywords=TRUE),"create")     
+  expect_equal(make.db.names(drv,"create",allow.keywords=FALSE),"[create]")  
+  
+})
+        
