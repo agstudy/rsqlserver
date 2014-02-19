fe47fdffe4930ac877f5a443e1f1dccd0e53f426
A.G <amingassem@gmail.com>
2014-02-19 20:05:12 +0100

    fix bug: better test for dbListtables(not yet optimal)

--- inst/tests/testthat/test-DBI-compliance.R
+++ inst/tests/testthat/test-DBI-compliance.R
@@ -5,6 +5,6 @@ test_that('dbListTable: get all tables',{
   url = "Server=localhost;Database=TEST_RSQLSERVER;Trusted_Connection=True;"
   conn <- dbConnect('SqlServer',url=url)
   res <- dbListTables(conn)
-  expect_is(res,"data.frame")
+  expect_true(length(res)>0)
   dbDisconnect(conn)
 })
