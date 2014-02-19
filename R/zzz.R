2344b88ccc6c6945ab89fc9d4283772443593195
A.G <amingassem@gmail.com>
2014-02-19 15:38:58 +0100

    move tests under inst to run test_package

--- R/zzz.R
+++ R/zzz.R
@@ -2,9 +2,8 @@
 
 .onLoad <- function(libname, pkgname){
   if(.Platform$OS.type == "windows") {
-    libLocation<- system.file(package=pkgname)
-   libpath <- file.path(libLocation, 'libs')
-   ## libpath <- file.path(libLocation)
+    libLocation <- system.file(package=pkgname)
+    libpath <- file.path(libLocation, 'libs')
     f <- file.path(libpath, 'rsqlserver.net.dll')
     if( !file.exists(f) ) {
       packageStartupMessage('Could not find path to rsqlserver.dll, 
---
+++ inst/tests/testthat/test-connections.R
@@ -0,0 +1,74 @@
+###
+context('Test connections')
+
+test_isConnected <- function(url){
+  url = "Server=localhost;Database=TEST_RSQLSERVER;Trusted_Connection=True;"
+  conn <- dbConnect('SqlServer',url=url)
+  expect_equal(dbGetInfo(conn,"State")[[1]],"1")
+  dbDisconnect(conn)
+}
+
+test_that("dbConnect : Standard Security",{
+ url = "Server=localhost;Database=TEST_RSQLSERVER;User Id=collateral;
+  Password=collat;"
+ test_isConnected(url)
+})
+
+###           
+test_that("dbConnect : Trusted Connection",{
+  url = "Server=localhost;Database=TEST_RSQLSERVER;Trusted_Connection=True;"
+  test_isConnected(url)
+  
+})
+# 
+# 
+# test_that("Connection to a SQL Server instance",{
+#   url = "Server=myServerName/myInstanceName;Database=myDataBase;User Id=myUsername;
+#   Password=myPassword;"
+#   con <- dbConnect('SqlServer',url=url)
+#   expect_equal(dbGetInfo(conn,"State"),0)
+#   dbDisconnect(url)
+#   
+# })
+# 
+# test_that("Trusted Connection from a CE device",{
+#  url = "Data Source=myServerAddress;Initial Catalog=myDataBase;Integrated Security=SSPI;
+#   User ID=myDomain/myUsername;Password=myPassword;"
+#  con <- dbConnect('SqlServer',url=url)
+#  expect_equal(dbGetInfo(conn,"State"),0)
+#  dbDisconnect(url)
+# })
+# 
+# test_that("Connect via an IP address",{
+#   url = "Data Source=190.190.200.100,1433;Network Library=DBMSSOCN;
+#   Initial Catalog=myDataBase;User ID=myUsername;Password=myPassword;"
+#   con <- dbConnect('SqlServer',url=url)
+#   expect_equal(dbGetInfo(conn,"State"),0)
+#   dbDisconnect(url)
+#   
+#   
+# })
+# 
+# test_that("Enable MARS",{
+#   url = "Server=myServerAddress;Database=myDataBase;Trusted_Connection=True;
+#   MultipleActiveResultSets=true;"
+#   con <- dbConnect('SqlServer',url=url)
+#   expect_equal(dbGetInfo(conn,"State"),0)
+#   dbDisconnect(url)
+# })
+# 
+# 
+# test_that("Attach a database file on connect to a local SQL Server Express instance",{
+#   url = "Server=./SQLExpress;AttachDbFilename=C:/MyFolder/MyDataFile.mdf;Database=dbname;
+# Trusted_Connection=Yes;"
+#   con <- dbConnect('SqlServer',url=url)
+#   expect_equal(dbGetInfo(conn,"State"),0)
+#   dbDisconnect(url)
+# })
+# 
+# 
+# 
+# 
+# 
+#           
+#           
