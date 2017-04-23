c290d2356aff1cb1ce01ce1b75f058ed516b025b
Ruaridh Williamson <ruaridh.williamson@icloud.com>
2017-04-23 12:27:04 +1000

    Clean test-connections

--- inst/tests/testthat/test-connections.R
+++ inst/tests/testthat/test-connections.R
@@ -1,64 +1,45 @@
-###
 context('Test connections')
 
-SERVER_ADDRESS <- "192.168.0.10"
-
+server <- "192.168.0.10"
+dbname <- "TEST_RSQLSERVER"
+user <- "collateral"
+password <- "Kollat"
 
 
 test_isConnected <- function(url){
   on.exit(dbDisconnect(conn))
   conn <- dbConnect('SqlServer',url=url)
   expect_equal(dbGetInfo(conn,"State")$State,"Open")
-
 }
 
 test_that("dbConnect : Standard Security",{
-  url = "Server=%s;Database=TEST_RSQLSERVER;User Id=collateral;Password=Kollat;"
-  url <- sprintf(url,SERVER_ADDRESS)
+  url <- "Server=%s;Database=%s;User Id=%s;Password=%s;"
+  url <- sprintf(url, server, dbname, user, password)
   test_isConnected(url)
 })
 
-###           
 test_that("dbConnect : Trusted Connection",{
-  if(.Platform$OS.type =="windows"){
-    url = "Server=localhost;Database=TEST_RSQLSERVER;Trusted_Connection=True;"
-    test_isConnected(url)
-  }
-  
+  skip_on_os(c("mac", "linux", "solaris"))
+  url <- "Server=localhost;Database=%s;Trusted_Connection=True;"
+  test_isConnected(url, dbname)
 })
 
 test_that("dbConnect : Connecting using with connection parameters",{
-  host=SERVER_ADDRESS
-  dbname="TEST_RSQLSERVER"
-  user="collateral"
-  password="Kollat"
-  conn <- dbConnect('SqlServer',host=host,dbname=dbname,user=user,password=password)
+  conn <- dbConnect('SqlServer',host=server,dbname=dbname,user=user,password=password)
   expect_equal(dbGetInfo(conn,"State")$State,"Open")
   dbDisconnect(conn)
 })
 
-
-
-
-
-test_that("dbConnect : TRUSTED Connection using with connection parameters ",{
-  if(.Platform$OS.type =="windows"){
-    host="localhost"
-    dbname="TEST_RSQLSERVER"
-    conn <- dbConnect('SqlServer',host=host,dbname=dbname,trusted=TRUE)
-    expect_equal(dbGetInfo(conn,"State")$State,"Open")
-    dbDisconnect(conn)
-  }
+test_that("dbConnect : TRUSTED Connection using with connection parameters",{
+  skip_on_os(c("mac", "linux", "solaris"))
+  conn <- dbConnect('SqlServer',host="localhost",dbname=dbname,trusted=TRUE)
+  expect_equal(dbGetInfo(conn,"State")$State,"Open")
+  dbDisconnect(conn)
 })
 
-test_that("dbConnect : choose parameter if url is NULL ",{
-  if(.Platform$OS.type =="windows"){
-    
-    host="localhost"
-    dbname="TEST_RSQLSERVER"
-    conn <- dbConnect('SqlServer',host=host,dbname=dbname,trusted=TRUE,url=NULL)
-    expect_equal(dbGetInfo(conn,"State")$State,"Open")
-    dbDisconnect(conn)
-  }
+test_that("dbConnect : choose parameter if url is NULL",{
+  skip_on_os(c("mac", "linux", "solaris"))
+  conn <- dbConnect('SqlServer',host="localhost",dbname=dbname,trusted=TRUE,url=NULL)
+  expect_equal(dbGetInfo(conn,"State")$State,"Open")
+  dbDisconnect(conn)
 })
-
