00a2f91a876d8aac91cba3e15ab5bb045f1f5cd5
Ruaridh Williamson <ruaridh.williamson@icloud.com>
2017-11-18 09:58:27 +0000

    Update Test Extra DBI

--- inst/tests/testthat/test-extradbi.R
+++ inst/tests/testthat/test-extradbi.R
@@ -1,30 +1,28 @@
-context('Test Extra Functions')
+context("Test Extra Functions")
 
-test_that("dbGetScalar : Returns null for NO_OBJECT",{
-  on.exit(  dbDisconnect(conn))
-  conn <- get_connection()
+test_that("dbGetScalar: Returns null for NO_OBJECT",{
+  on.exit(dbDisconnect(conn))
+  conn <- get_con()
   req <- "SELECT OBJECT_ID('NO_OBJECT','S') AS 'Object ID';"
   res <- dbGetScalar(conn,req)
-  expect_true(is.null(res))
+  expect_null(res)
 })
 
-test_that("dbGetScalar : Returns a scalar",{
-  on.exit(  dbDisconnect(conn))
-  conn <- get_connection()
+test_that("dbGetScalar: Returns a scalar",{
+  on.exit(dbDisconnect(conn))
+  conn <- get_con()
   req <- "SELECT OBJECT_ID('sys.sysprivs','S') AS 'Object ID';"
   res <- dbGetScalar(conn,req)
-  expect_true(length(res)==1)
+  expect_length(res, 1)
 })
 
-test_that("dbConnect : We can set a timeout",{
-  on.exit(  dbDisconnect(conn))
-  conn <- get_connection()
-  query <- "select name from sys.tables"
-  rs <- dbSendQuery(conn, query,timeout=90)
-  df <- fetch(rs,-1)
-  if(.Platform$OS.type =="windows"){
-    expect_equal(as.integer(dbGetInfo(rs,'TimeOut')),90)
-  }
+test_that("dbConnect: Set a timeout",{
+  skip_on_os(c("mac", "linux", "solaris"))
+  on.exit(dbDisconnect(conn))
+  conn <- get_con()
+  query <- "SELECT name FROM sys.tables"
+  rs <- dbSendQuery(conn, query, timeout = 90)
+  df <- fetch(rs, -1)
+  expect_equal(as.integer(dbGetInfo(rs, "TimeOut")), 90)
   dbClearResult(rs)
 })
-
