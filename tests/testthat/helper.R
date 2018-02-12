9be1fb87f6d329a68f6c5ad7bc71205064233546
Ruaridh Williamson <ruaridh.williamson@icloud.com>
2018-02-12 16:30:16 +0000

    Fix travis docker connections

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -5,17 +5,21 @@ dbname <- "rsqlserverdb"
 user <- "SA"
 password <- "Password12!"
 
-set_url <- function(){
-  if (identical(Sys.getenv("TRAVIS"), "true")) {
-    "Server=mydb;Database=rsqlserverdb;User ID=sa;Password=Password12!"
-  } else if (identical(Sys.getenv("APPVEYOR"), "True")) {
-    "Server=(local)\\SQL2014;Database=rsqlserverdb;User ID=sa;Password=Password12!"
-  } else {
-    sprintf("Server=%s;Database=%s;User Id=%s;Password=%s;",
-            server, dbname, user, password)
-  }
+if (identical(Sys.getenv("TRAVIS"), "true")) {
+  server <- "mssqldb"
+  dbname <- "rsqlserverdb"
+  user <- "sa"
+  password <- "Password12!"
+} else if (identical(Sys.getenv("APPVEYOR"), "True")) {
+  server <- "(local)\\SQL2014"
+  dbname <- "rsqlserverdb"
+  user <- "sa"
+  password <- "Password12!"
 }
 
+url <- sprintf("Server=%s;Database=%s;User Id=%s;Password=%s;",
+               server, dbname, user, password)
+
 get_con <- function(){
-  dbConnect("SqlServer", url = set_url())
+  dbConnect("SqlServer", url = url)
 }
